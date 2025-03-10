cdd577b76d42f00d0c327e0475fc19b374325cc1
Carlos Dobler <dobler.carlos@gmail.com>
2025-03-10 14:20:34 -0600

    Update nex_table.R

--- nex_table.R
+++ nex_table.R
@@ -1,18 +1,22 @@
 library(tidyverse)
 
+
 file_index <- c("https://nex-gddp-cmip6.s3-us-west-2.amazonaws.com/index_md5.txt",
                 "https://nex-gddp-cmip6.s3-us-west-2.amazonaws.com/index_v1.1_md5.txt",
                 "https://nex-gddp-cmip6.s3-us-west-2.amazonaws.com/index_v1.2_md5.txt")
 
 
+
 df <-
   # read tables
   map_dfr(file_index, read_delim, delim = " ", col_names = FALSE) |> 
-
   # keep only filenames
   select(X3) |> 
-  rename("path" = 1) |> 
+  rename("path" = 1)
 
+
+df <- 
+  df |> 
   # format
   separate(path, into = c("project", "model", "scenario", "run", "variable", "filename"), sep = "/") |>
   select(model, scenario, variable, filename) |> 
@@ -20,7 +24,9 @@ df <-
   # add version
   mutate(version = if_else(str_detect(filename, "_v\\d+\\.\\d+"), 
                           as.numeric(str_extract(filename, "\\d+\\.\\d+")),
-                          1.0)) |> 
+                          1.0),
+         
+         year = as.numeric(str_extract(filename, "\\d{4}(?=\\.nc)"))) |> 
 
   # add path
   bind_cols(df) |> 
