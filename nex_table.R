998ec8d79cf44afca3ba568152da9256a4ac175a
Carlos Dobler <dobler.carlos@gmail.com>
2025-03-10 14:11:18 -0600

    Create nex_table.R

---
+++ nex_table.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+
+file_index <- c("https://nex-gddp-cmip6.s3-us-west-2.amazonaws.com/index_md5.txt",
+                "https://nex-gddp-cmip6.s3-us-west-2.amazonaws.com/index_v1.1_md5.txt",
+                "https://nex-gddp-cmip6.s3-us-west-2.amazonaws.com/index_v1.2_md5.txt")
+
+
+df <-
+  # read tables
+  map_dfr(file_index, read_delim, delim = " ", col_names = FALSE) |> 
+
+  # keep only filenames
+  select(X3) |> 
+  rename("path" = 1) |> 
+
+  # format
+  separate(path, into = c("project", "model", "scenario", "run", "variable", "filename"), sep = "/") |>
+  select(model, scenario, variable, filename) |> 
+  
+  # add version
+  mutate(version = if_else(str_detect(filename, "_v\\d+\\.\\d+"), 
+                          as.numeric(str_extract(filename, "\\d+\\.\\d+")),
+                          1.0)) |> 
+
+  # add path
+  bind_cols(df) |> 
+  
+  # filter latest version
+  group_by(model, scenario, variable) |> 
+  filter(version == max(version)) |> 
+  ungroup()
